318558cfc62833a7f37650b002191536ce772b88
CINTIA-GIT <cfernandezl@viewnext.com>
2024-12-03 09:47:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen)
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
