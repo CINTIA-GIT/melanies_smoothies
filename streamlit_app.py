c92280e4d5964c0322110168ef80d552d1315f48
CINTIA-GIT <cfernandezl@viewnext.com>
2024-12-03 09:33:55 +0100

    Update streamlit_app.py
    add a Python package library called requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
     st.write(ingredients_string)
+
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """')"""
@@ -34,3 +35,7 @@ if ingredients_list:
     if time_to_insert: 
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
